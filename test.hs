59ce33edb1fdeeb0faf27f6eded20ecb8af739e0
ekaschalk <ekaschalk@gmail.com>
2017-03-17 20:58:31 -0700

    Notes: Lists II (map)

--- test.hs
+++ test.hs
@@ -1,12 +1,17 @@
-fac n = product [1..n]
+-- Summing a List
+mysum :: [Int] -> Int
+mysum xs
+  | xs == [] = 0
+  | otherwise = head xs + mysum (tail xs)
 
-absolute x
-  | x < 0 = negate x
-  | otherwise = x
+-- Pattern matching version
+mysum2 :: [Int] -> Int
+mysum2 [] = 0
+mysum2 xs = head xs + mysum2 (tail xs)
 
-factorial 0 = 1
-factorial n = n * factorial (n-1)
+-- Using cons in pattern matching
+mysum3 :: [Int] -> Int
+mysum3 [] = 0
+mysum3 (x:xs) = x + mysum3 xs
 
-mylength :: [a] -> Int
-mylength []     = 0
-mylength (x:xs) = 1 + mylength xs
+-- xxx
