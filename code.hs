deca0e03b41610fceaa1dfb60c77b7570658e403
ekaschalk <ekaschalk@gmail.com>
2017-04-22 13:28:19 -0700

    Typeclass creation exercises, 6.9-6.11

--- code.hs
+++ code.hs
@@ -59,3 +59,76 @@ xform' (x, y) = (,) (xz x) $ yz y
 
 munge :: (x -> y) -> (y -> (w, z)) -> x -> w
 munge f g = fst . g . f
+
+-- Chapter 6
+
+data DayOfWeek =
+  Mon | Tue | Weds | Thu | Fri | Sat | Sun
+
+data Date =
+  Date DayOfWeek Int
+
+instance Eq DayOfWeek where
+  (==) Mon Mon = True
+  (==) _ _ = False
+
+instance Eq Date where
+  (==) (Date weekday monthNum)
+       (Date weekday' monthNum') =
+    weekday == weekday' && monthNum == monthNum'
+
+x = Date Mon 10 == Date Mon 10
+
+--
+
+data TisAnInteger =
+  TisAn Integer
+
+instance Eq TisAnInteger where
+  (==) (TisAn x)
+       (TisAn y) =
+    x == y
+
+data TwoIntegers =
+  Two Integer Integer
+
+instance Eq TwoIntegers where
+  (==) (Two x y)
+       (Two x' y') =
+    x == y && x' == y'
+
+data StringOrInt =
+    TisAnInt Int
+  | TisAString String
+
+instance Eq StringOrInt where
+  TisAnInt x == TisAnInt y = x == y
+  TisAString x == TisAString y = x == y
+
+data Pair a =
+  Pair a a
+
+instance Eq a => Eq (Pair a) where
+  Pair x y == Pair x' y' = x == x'
+
+data Tuple a b =
+  Tuple a b
+
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  Tuple x y == Tuple x' y' = x == x' && y == y'
+
+data Which a =
+    ThisOne a
+  | ThatOne a
+
+instance Eq a => Eq (Which a) where
+  ThisOne x == ThisOne y = x == y
+  ThatOne x == ThatOne y = x == y
+
+data EitherOr a b =
+    Hello a
+  | Goodbye b
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  Hello x == Hello y = x == y
+  Goodbye x == Goodbye y = x == y
