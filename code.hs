a99db383be92d71704c1d0ade49e544222a9d85f
ekaschalk <ekaschalk@gmail.com>
2017-04-22 16:12:44 -0700

    Chapter 7 complete

--- code.hs
+++ code.hs
@@ -151,3 +151,16 @@ chk f a b = f a == b
 arith :: Num b => (a -> b) -> Integer -> a -> b
 arith f x a = f a + fromInteger x
 -- arith (\x -> x+1) 2 3.5
+
+-- Chp 7
+
+k (x, y) = x
+k1 = k ((4-1), 10)
+k2 = k ("hi", (1+2))
+k3 = k (3, True)
+
+fo :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
+fo (a, _, c) (d, _, f) = ((a, d), (c, f))
+
+g :: (a -> b) -> (a, c) -> (b, c)
+g f (a, c) = (f a, c)
