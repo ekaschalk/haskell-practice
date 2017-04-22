1ddfd31d232a40f1703eb98c606d0afe2359d1d1
ekaschalk <ekaschalk@gmail.com>
2017-04-21 23:07:37 -0700

    Chapter 5 all chapter exercises

--- code.hs
+++ code.hs
@@ -1,2 +1,61 @@
--- Chapter 1
-(+)
+-- Chapter 5
+
+func1 :: [a] -> a
+func1 (x:_) = x
+
+func2 :: Ord a => a -> a -> Bool
+func2 x y = if (x > y) then True else False
+
+func3 :: (a, b) -> b
+func3 (x, y) = y
+
+i :: a -> a
+i = id
+
+c :: a -> b -> a
+c a b = a
+
+c'' :: b -> a -> b
+c'' a b = a
+
+c' :: a -> b -> b
+c' a b = b
+
+-- r -> drop/take
+
+co :: (b -> c) -> (a -> b) -> (a -> c)
+co bc ab = bc . ab
+
+a :: (a -> c) -> a -> a
+a f a = a
+
+a' :: (a -> b) -> a -> b
+a' f a = f a
+
+f :: Int -> String
+f = undefined
+
+g :: String -> Char
+g = undefined
+
+h :: Int -> Char
+h = g . f
+
+data X
+data Y
+data Z
+
+xz :: X -> Z
+xz = undefined
+
+yz :: Y -> Z
+yz = undefined
+
+xform :: (X, Y) -> (Z, Z)
+xform xy = (,) (xz $ fst xy) (yz $ snd xy)
+
+xform' :: (X, Y) -> (Z, Z)
+xform' (x, y) = (,) (xz x) $ yz y
+
+munge :: (x -> y) -> (y -> (w, z)) -> x -> w
+munge f g = fst . g . f
