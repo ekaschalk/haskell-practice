52b71db83e7761ca69a8927d2b6e3ebc2178d45e
ekaschalk <ekaschalk@gmail.com>
2017-04-22 21:16:06 -0700

    9 - 9.6 complete

--- code.hs
+++ code.hs
@@ -1,1 +1,20 @@
 -- chp9
+myEnumFromTo :: Enum a => a -> a -> [a]
+myEnumFromTo x y = take (fromEnum y) $ iterate succ x
+-- myEnumFromTo 1 5 == [1..5]
+
+splitStr :: String -> [String]
+splitStr sentence = split1 sentence []
+  where split1 sentence words
+          | null sentence = reverse words
+          | otherwise = split1 (dropWord sentence) $ getWord sentence : words
+          where getWord = takeWhile (/= ' ')
+                dropWord = drop 1 . dropWhile (/= ' ')
+-- splitStr "hello doggy run away" == ["hello","doggy","run","away"]
+
+joinStr :: String -> String
+joinStr sentence = map addNewLine sentence where
+  addNewLine x
+    | x == ' ' = '\n'
+    | otherwise = x
+-- joinStr "hello doggy run away" == "hello\ndoggy\nrun\naway"
