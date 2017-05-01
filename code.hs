176429aac3496d8cb3aa52b314f81d2ed6542ff6
ekaschalk <ekaschalk@gmail.com>
2017-05-01 00:27:05 -0700

    chp11 exercises up until final set complete

--- code.hs
+++ code.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 import Data.List
+import Data.Char
 import Data.Function (on)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Ord
 
 -- import Data.Foldable
 
@@ -143,12 +148,95 @@ postorder :: BinaryTree a -> [a]
 postorder Leaf = []
 postorder (Node left x right) = postorder left ++ postorder right ++ [x]
 
---           x   left  right
-foldTree :: (a -> b -> b -> b) -> b -> BinaryTree a -> b
--- foldTree _ i Leaf = Leaf
--- foldTree acc i = foldMap acc inorder
-foldTree acc i Leaf = i
-foldTree acc i (Node left x right) = acc x
-                                     (foldTree acc i left)
-                                     (foldTree acc i right)
--- the i part is wrong, is doing numleaf times
+-- foldTree :: (a -> b -> b -> b) -> b -> BinaryTree a -> b
+-- foldTree acc i Leaf = i
+-- foldTree acc i (Node left x right) = acc x
+--                                      (foldTree acc i left)
+--                                      (foldTree acc i right)
+
+-- foldTree :: (a -> b -> b -> b) -> b -> BinaryTree a -> b
+-- foldTree acc i tree = go acc tree
+--   where go _ Leaf = undefined
+--         go acc (Node left x right) = acc x (go acc left) (go acc right)
+
+-- foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+-- foldTree acc i = foldr acc i . inorder
+
+-- mapTree' :: (a -> b) -> BinaryTree a -> BinaryTree b
+-- mapTree' f = foldTree (f . (:))
+
+isSub :: (Eq a) => [a] -> [a] -> Bool
+isSub (x:[]) (y:[]) = x == y
+isSub (x:xs) (y:ys) = if x == y then isSub xs ys else isSub (x:xs) ys
+isSub _ (y:_) = True
+isSub _ _ = False
+
+capitalizeWord :: String -> String
+capitalizeWord (x:xs) = toUpper x:xs
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords = map (\(x:xs) -> (x:xs, toUpper x:xs)) . words
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph [] = ""
+capitalizeParagraph xs = capitalizeWord $ take sentence xs ++
+                         (capitalizeParagraph $ drop sentence xs)
+  where sentence = adjustIndex $ Data.List.findIndex (== '.') xs
+        adjustIndex (Just x) = 2 + x
+        adjustIndex _ = length xs
+
+-- Phone exercise
+
+-- data Phone = Phone (Map Char String) deriving Show
+
+-- phone :: Phone
+-- phone = Phone (Map.fromList [('1', ""), ('2', "ABC"), ('3', "DEF"),
+--                              ('4', "GHI"), ('5', "JKL"), ('6', "MNO"),
+--                              ('7', "PQRS"), ('8', "TUV"), ('9', "WXYZ"),
+--                              ('*', "^"), ('0', "+_"), ('#', ".,")])
+
+-- data Phone = Map Char String
+
+phone :: Map Char String
+phone = Map.fromList [('1', ""), ('2', "ABC"), ('3', "DEF"),
+                       ('4', "GHI"), ('5', "JKL"), ('6', "MNO"),
+                       ('7', "PQRS"), ('8', "TUV"), ('9', "WXYZ"),
+                       ('*', "^"), ('0', "+_"), ('#', ".,")]
+
+type Digit = Char
+type Presses = Int
+
+pressed :: Map Char String -> Char -> (Digit, Presses)
+pressed p digit = (k, v)
+  where pair = Map.elemAt 0 $ Map.filter (elem digit) p
+        k = fst pair
+        v = (+ 1) $ Data.Maybe.fromJust $ findIndex (== digit) $ snd pair
+
+-- cellPhonesDead :: Map Char String -> String -> [(Digit, Presses)]
+-- cellPhonesDead p = map (pressed p)
+
+-- cellPhonesDead phone "MNOPQERS"
+-- [('6',1),('6',2),('6',3),('7',1),('7',2),('3',2),('7',3),('7',4)]
+
+fingerTaps :: [(Digit, Presses)] -> Presses
+fingerTaps = Map.fold (+) 0 . Map.fromListWith (+)
+
+reverseTaps :: Map Char String -> Char -> [(Digit, Presses)]
+reverseTaps p digit = res
+  where pair = Map.elemAt 0 $ Map.filter (elem . toUpper $ digit) p
+        k = fst pair
+        v = (+ 1) $ Data.Maybe.fromJust $ findIndex (== toUpper digit) $ snd pair
+        fstPair = (k, v)
+        res = if isUpper digit then [fstPair, ('*', 1)] else [fstPair]
+
+cellPhonesDead :: Map Char String -> String -> [(Digit, Presses)]
+cellPhonesDead p = concat . map (reverseTaps p)
+
+popularestDigit :: Map Char String -> String -> Char
+popularestDigit p = fst . maximumBy (comparing snd) .
+                    Map.toList . Map.fromListWith (+) . cellPhonesDead p
+
+coolestChar :: String -> Char
+coolestChar = flip (!!) 0 . maximumBy (comparing length) . groupBy (==) . sort
+
+-- Above should work as is for coolestWord as well
