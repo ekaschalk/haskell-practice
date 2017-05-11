0e079e81da78c123a5a7493e70542bdcfc548dd6
ekaschalk <ekaschalk@gmail.com>
2017-05-10 23:35:23 -0700

    chp11 all maybe exerises, now either

--- code.hs
+++ code.hs
@@ -1,7 +1,7 @@
 import Control.Applicative
 import Data.Traversable (sequenceA)
 import Data.List (tails)
-import Data.Maybe (maybe, mapMaybe)
+import Data.Maybe (maybe, mapMaybe, fromMaybe)
 
 -- chp12
 
@@ -36,3 +36,49 @@ mkWord :: String -> Maybe Word'
 mkWord x = if vowels > consonants then Just (Word' x) else Nothing
   where vowels = countVowels x
         consonants = toInteger (length x) - vowels
+
+data Nat =
+    Zero
+  | Succ Nat
+  deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero = 0
+natToInteger (Succ x) = 1 + natToInteger x
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat x
+  | x == 0 = Just Zero
+  | x >= 0 = Just (Succ (fromMaybe Zero $ integerToNat $ x-1))
+  | x < 0 = Nothing
+
+
+isJust :: Maybe a -> Bool
+isJust (Just x) = True
+isJust _ = False
+
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee b f (Just a) = f a
+mayybee b _ _ = b
+
+fromMaybe' :: a -> Maybe a -> a
+fromMaybe' a (Just b) = b
+fromMaybe' a _ = a
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe (x:_) = Just x
+listToMaybe _ = Nothing
+
+maybeToList :: Maybe a -> [a]
+maybeToList (Just x) = [x]
+maybeToList _ = []
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes = concat . map maybeToList
+
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe x = if any isNothing x then Nothing else Just (catMaybes x)
