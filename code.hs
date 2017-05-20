23457564c42f774856ad5528fc96233f76365497
ekaschalk <ekaschalk@gmail.com>
2017-05-19 19:45:09 -0700

    ch15 read, start chapter exercises

--- code.hs
+++ code.hs
@@ -1,1 +1,20 @@
 -- chp15
+
+import Data.Monoid hiding ((<>))
+import Data.Semigroup
+
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
+-- instance Monoid a => Monoid (Optional a) where
+--   -- mempty (Only a) = mempty a
+--   mempty = Nada
+--   mappend (Only a) (Only b) = Only $ a <> b
+--   mappend (Only a) Nada = Only $ a <> mempty a
+--   mappend _ (Only b) = Only $ b <> mempty b
+--   mappend _ _ = Nada
+
+-- Only (Sum 1) <> Only (Sum 2)
